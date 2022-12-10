d20c727f55c9b57ed5f280dadf947fa1db4b3968
alxdrcirilo <alxdrcirilo@outlook.com>
2022-12-10 15:40:10 +0100

    Updated plots

--- tictactoe/plot.py
+++ tictactoe/plot.py
@@ -5,37 +5,42 @@ import seaborn as sns
 data = pd.read_csv(filepath_or_buffer="tournament.csv")
 
 # General statistics
-p = sns.catplot(
-    data=data,
-    kind="bar",
-    x="result",
-    y="value",
-    row="depth",
-    col="size",
-    hue="mode",
-)
-p.savefig("stats.png", dpi=200)
+with sns.plotting_context("notebook", font_scale=1.5):
+    p = sns.catplot(
+        data=data,
+        kind="bar",
+        x="result",
+        y="value",
+        row="depth",
+        col="size",
+        hue="mode",
+        height=4,
+        aspect=1.5,
+    )
+    p.savefig("stats.png")
 
 # Runtimes
 fig, ax = plt.subplots(figsize=(8, 4))
-sns.scatterplot(
+data["size"] = data["size"].astype(int)
+p = sns.scatterplot(
     data=data,
     x="size",
     y="runtime",
-    hue="depth",
-    style="mode",
+    hue="mode",
+    style="depth",
     palette="Set1",
     ax=ax,
 )
-sns.lineplot(
+p = sns.lineplot(
     data=data,
     x="size",
     y="runtime",
-    hue="depth",
-    style="mode",
+    hue="mode",
+    style="depth",
     palette="Set1",
-    linewidth=0.5,
     legend="",
     ax=ax,
 )
-fig.savefig("runtimes.png", dpi=200)
+p.set(xticks=data["size"].unique())
+sns.despine()
+fig.savefig("runtimes.png")
